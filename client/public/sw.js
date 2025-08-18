27e999233194845d1b3517a021f166f202c0c14c
bembeautypro <paolo@bembeauty.com.br>
2025-08-18 17:04:08 -0300

    Create sw.js

---
+++ client/public/sw.js
@@ -0,0 +1,4 @@
+// client/public/sw.js
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', () => {}); // passthrough
