36d172a57e8256b3fddad698f03dab03c55630dd
bembeautypro <paolo@bembeauty.com.br>
2025-08-25 12:34:56 -0300

    Update sw.js

--- client/public/sw.js
+++ client/public/sw.js
@@ -3,3 +3,5 @@ self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', (event) => {
   event.waitUntil(self.clients.claim());
 });
+
+// Sem fetch handler -> o navegador cuida de tudo (sem overhead/avisos)
